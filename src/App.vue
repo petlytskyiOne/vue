590ab0627bbf56c8b5e0ab5b06bf9896a3502220
Віталій Миколайович Петлицький <Vitalii.Petlytskyi@forum-am.in>
2024-04-11 16:26:25 +0300

    commit Auto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      info: "Title!",
+      someInfo: 'Anons of message'
+    }
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+h3 {
+  font-weight: lighter;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+p {
+  color: rgb(203, 31, 31);
 }
 </style>
